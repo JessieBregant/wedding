24c713c06d02d5d73bd17961f9295db850b80de4
Bob <bob@bobthesecurityguy.com>
2016-06-23 23:23:49 +0000

    cleanup!
    Cleaning up some old stuff.

--- js/settings.js
+++ js/settings.js
@@ -21,7 +21,7 @@
 
 		// Smooth Scroll on Menu Click
 
-		$('.navbar a[href^=#]').on("click",function(){
+		$('.navbar a[href^="#"]').on("click",function(){
 			var t= $(this.hash);
 			var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
 			if(t.length){
@@ -46,63 +46,10 @@
 
 		// Countdown
 		
-		//var weddingDate = new Date();
-		//var weddingDate = new Date(weddingDate.getFullYear() + 1, 1 - 1, 1);
 		var weddingDate = new Date("Oct 08, 2016 16:00:00")
 		$(".countdown").countdown({
 			until: weddingDate,
 			format: 'ODHMS'
-		});
-
-		// ====================================================================
-
-		// Owl Carousel
-
-		$("#registry .owl-carousel").owlCarousel({
-			items: 6,
-			itemsDesktop: [1199,5],
-			itemsDesktopSmall: [991,4],
-			itemsTablet: [767,3],
-			slideSpeed: 800
-		});
-
-		// ====================================================================
-
-		// Parallax
-
-		$('.parallax').scrolly({bgParallax: true});
-
-		// ====================================================================
-
-		// Fun Facts Counter
-
-		var flag = 0;
-
-	    $(window).scroll(function() {
-	        if (flag == 1){
-	           return false;
-	        }
-	        else{
-	           var bh = $(window).height();
-	           var st = $(window).scrollTop();
-	           var el = $('.timer');
-	           var eh = el.height();
-	           if ( st >= (100 + eh) - bh ) {
-	               el.countTo({
-	                   speed: 2000,
-	                   refreshInterval: 20
-	               });
-	           }
-	           flag = 1;
-	        }
-	    });
-
-		// ====================================================================
-
-		// Fancybox
-
-		$('.fancybox').fancybox({
-			openEffect: 'none'
 		});
 
 		// ====================================================================
